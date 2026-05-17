3a9cb7ee23a321e395aeef795d4212601e54c186
toobarashid64 <toobarashid644@gmail.com>
2026-05-17 19:00:07 +0500

    code.py

--- code.py
+++ code.py
@@ -1,1 +1,11 @@
-print('Hello!World')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    # This is the exact message displayed in the video
+    return 'Hello World! welcome to Tech capture from standard environment'
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
